50cc95d49ca63dc383003e6e1f59d633c05abae1
krusehsbi <alexander.kruse@hsbi.de>
2025-11-10 15:49:58 +0100

    added 0shot yoloworld

---
+++ baselines/yoloworld/0shot.py
@@ -0,0 +1,19 @@
+from ultralytics import YOLO
+import yaml
+
+# Create a YOLO-World model
+model = YOLO("yolov8s-world.pt")  # or select yolov8m/l-world.pt for different sizes
+
+with open('../../datasets/1shot/dataset.yaml', 'r') as f:
+    data = yaml.safe_load(f)
+
+# Extract class names
+class_names = list(data['names'].values()) if isinstance(data['names'], dict) else data['names']
+
+print("Classes:", class_names)
+
+model.set_classes(class_names)
+
+# Conduct model validation on the COCO8 example dataset
+metrics = model.val(data="../../datasets/1shot/dataset.yaml", split="test", imgsz=1024,
+                    batch=8, project="runs/yoloworld", name="0shot", save_json=True)

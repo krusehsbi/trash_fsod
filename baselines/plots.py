9f5f41147e0f82a7881019ab43357248d8da7be6
krusehsbi <alexander.kruse@hsbi.de>
2025-11-26 13:04:57 +0100

    sdxl plot

--- baselines/plots.py
+++ baselines/plots.py
@@ -5,6 +5,7 @@ k_values = [1, 3, 5, 10, 30]
 
 yolo = [0.108, 0.111, 0.138, 0.166, 0.213]
 rtdetr = [0.142, 0.138, 0.143, 0.202, 0.283]
+rtdetr_sdxl = [0.131, 0.155, 0.162, 0.207, 0.214]
 
 # Create evenly spaced positions for x-axis
 x = range(len(k_values))   # [0,1,2,3]
@@ -18,6 +19,23 @@ plt.plot(x, rtdetr, marker='x', linestyle='--',label="RT-DETR")
 plt.xticks(x, k_values)
 
 plt.title("Comparison of mAP50 for RT-DETR and YOLOv11 finetuned on k real objects.")
+plt.xlabel("k")
+plt.ylabel("mAP50")
+plt.grid(True)
+plt.legend()
+
+plt.tight_layout()
+plt.show()
+
+plt.figure(figsize=(8, 5))
+
+plt.plot(x, rtdetr, marker='x', linestyle='--', label="without SDXL")
+plt.plot(x, rtdetr_sdxl, marker='x', linestyle='--',label="with SDXL")
+
+# Replace x-axis numbers with actual k labels
+plt.xticks(x, k_values)
+
+plt.title("Comparison of mAP50 for RT-DETR with and without SDXL augmentations.")
 plt.xlabel("k")
 plt.ylabel("mAP50")
 plt.grid(True)
